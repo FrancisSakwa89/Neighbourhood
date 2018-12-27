00ba0d032cb634e3639d1a1802d84c812221ac5f
FrancisSakwa89 <sakwafrancis89@gmail.com>
2018-12-27 09:22:50 +0300

    configured urls in neighbour urls.py for navigation

--- Neighbor/settings.py
+++ Neighbor/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'Neigh.apps.NeighConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
