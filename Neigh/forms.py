0706773c1bdcaf5c996f67cc98b119d25e9e8e0f
FrancisSakwa89 <sakwafrancis89@gmail.com>
2018-12-28 13:16:33 +0300

    created forms.py file for the users to use in filling intheir credentials

---
+++ Neigh/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Profile, Neigh
+
+class NeighForm(forms.ModelForm):
+  class Meta:
+    model = Project
+    exclude = ['poster','postername', 'pub_date']
+
+
+
+
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ['user']
+
+class NeighLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')    
