7d4e6b21f2a432c3254c6f53857f204a6a506739
FrancisSakwa89 <sakwafrancis89@gmail.com>
2018-12-30 15:45:55 +0300

    added class contacts to admin.py

--- Neigh/admin.py
+++ Neigh/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Neighbourhood,Profile,Business
+from .models import Neighbourhood,Profile,Business, Contact
 
 admin.site.register(Neighbourhood)
 admin.site.register(Profile)
 admin.site.register(Business)
+admin.site.register(Contact)
